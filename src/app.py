248987b3efeffc3bb5e3423f19ce4a38c92a6e05
James <jewachu26@gmail.com>
2025-08-04 23:57:11 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -32,8 +32,12 @@ def batched(iterable, n_cols):
 
 @st.cache_resource
 def connect_to_gsheets():
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
     # Use credentials from Streamlit secrets
-    creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+    creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scopes)
     gc = gspread.authorize(creds)
     _sh = gc.open_by_key(SPREADSHEET_ID)  # optional: move SPREADSHEET_ID to secrets too
     return _sh
