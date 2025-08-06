1fc0b016711e4df6e978b763fac76e7d44565519
James <jewachu26@gmail.com>
2025-08-06 16:15:16 +0000

    update app

--- src/app.py
+++ src/app.py
@@ -304,7 +304,7 @@ def plot_sparkline(data):
     fig_spark.update_layout(
         showlegend=False,
         plot_bgcolor="white",
-        height=50,
+        height=200,
         margin=dict(t=10, l=0, b=0, r=0, pad=0),
     )
     return fig_spark
